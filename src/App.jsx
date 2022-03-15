482a42ca15a8a9f8b3a6a3f8cb880d56a0292261
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-03-15 16:54:52 -0400

    Update 2022-03-15 4:52 PM

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const Contact = lazy(() => import('./pages/Contact'));
 
 export default function App() {
   return  <Suspense fallback={<>...</>}> <Routes>
-      <Route /*basename="/portfolio-React"*/ path="/portfolio-React" element={<Layout />}/>
+      <Route basename="/portfolio-React" path="/portfolio-React" element={<Layout />}/>
         <Route index element={<Home />}/>
         <Route path="ProjectsMainPage" element={<ProjectsMainPage />}/>
         <Route path="Project1" element={<Project1 />}/>
