56e3e3c10ba8ebed86cef20b95cae4157638369b
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-03-18 16:35:10 -0400

    Update 2022-03-18 4:35 PM

--- src/components/ContactComp.jsx
+++ src/components/ContactComp.jsx
@@ -18,17 +18,17 @@ export default function ContactComp() {
                         "\nDescription: " + description.value  );
     }
     // verify name
-    const validerNom = () => {
-        // variables
-        const form = document.getElementById('contactForm');
-        const nom = document.getElementById('name');
-        const nomErreur = document.getElementById('nom-erreur');
+    const validateName = () => {
 
-        // objet validity, vérifie si l'input est valide
-        // Enlève la classe active (style.css)
+        const nom = document.getElementById('name');
+        const nomErreur = document.getElementById('nomerreur');
+        
+        // objet validity, vérifie si l'input est valide, enlève la classe active (style.css)
         if(nom.validity.valid){ 
             nomErreur.classList.remove('active'); 
-            printForm();
+            nomErreur.classList.add('tohide');
+            nom = ''; 
+            return true;
         }
         // si l'input n'est pas valide, ajoute la classe active (style.css)
         else{  
@@ -36,45 +36,127 @@ export default function ContactComp() {
             // si le champ est vide
             // valueMissing, tooShort, tooLong = propriété de validation
             if(nom.validity.valueMissing){
-                nomErreur.innerText = 'Veuillez entrer votre nom complet.';
+                nomErreur.innerText = 'Your name is missing, please enter your full name.';
+                nom = ''; 
             }
             else if(nom.validity.tooShort){
-                nomErreur.innerText = 'Votre nom est trop court.';
+                nomErreur.innerText = 'Your name is too short, please enter a name between 1 to 30 characters';
+                nom = ''; 
             }
             else if(nom.validity.tooLong){
-                nomErreur.innerText = 'Votre nom est trop long.';
+                nomErreur.innerText = 'Your name is too long, please enter a name between 1 to 30 characters.';
+                nom = ''; 
             }
         }
     }
-    
+    // validate email
+    const validateEmail = () => {
+        const courriel = document.getElementById('email');
+        const courrielErreur = document.getElementById('nomerreur');
+
+        // objet validity, vérifie si l'input est valide, enleve la classe active (style.css)
+        if(courriel.validity.valid){ 
+            courrielErreur.classList.remove('active'); 
+            //courrielErreur.classList.add('tohide'); 
+            return true;
+        }
+        // si l'input n'est pas valide, ajoute la classe active (style.css)
+        else{  
+            courrielErreur.classList.add('active'); 
+            // si le champ est vide
+            if(courriel.validity.valueMissing){
+                courrielErreur.innerText = 'Your email is missing, please enter your full email.';
+            }
+            // si un @ est manquant
+            else if(courriel.validity.typeMismatch){
+                courrielErreur.innerText = 'Please enter a valid email format.';
+            }
+        }
+    }
+    // verify subject
+    const validateSubject = () => {
+        const subject = document.getElementById('subject');
+        const subjectErreur = document.getElementById('nomerreur');
+
+        // objet validity, vérifie si l'input est valide, enlève la classe active (style.css)
+        if(subject.validity.valid){ 
+            subjectErreur.classList.remove('active'); 
+            //nomErreur.classList.add('tohide'); 
+            return true;
+        }
+        // si l'input n'est pas valide, ajoute la classe active (style.css)
+        else{  
+            subjectErreur.classList.add('active');  
+            // si le champ est vide
+            // valueMissing, tooShort, tooLong = propriété de validation
+            if(subject.validity.valueMissing){
+                subjectErreur.innerText = 'The subject is missing, please enter it.';
+            }
+            else if(subject.validity.tooLong){
+                subjectErreur.innerText = 'The subject is too long, please enter a subject between 1 to 30 characters.';
+            }
+        }
+    } 
+    // validate description    
+    const validateDescription = () => {
+        const description = document.getElementById('description');
+        const descriptionErreur = document.getElementById('nomerreur');
+        // objet validity, vérifie si l'input est valide, enlève la classe active (style.css)
+        if(description.validity.valid){ 
+            descriptionErreur.classList.remove('active'); 
+            //nomErreur.classList.add('tohide'); 
+            return true;
+        }
+        // si l'input n'est pas valide, ajoute la classe active (style.css)
+        else{  
+            descriptionErreur.classList.add('active');  
+            // si le champ est vide
+            // valueMissing, tooShort, tooLong = propriété de validation
+            if(description.validity.valueMissing){
+                descriptionErreur.innerText = 'The description is missing, please enter it.';
+            }
+            else if(description.validity.tooLong){
+                descriptionErreur.innerText = 'The  description is too long.';
+            }
+        }
+    }      
+    // validate all inputs
+    const validateInputs = () => {
+        // if all inputs are valid, print info into the console
+        if(validateName() && validateEmail() && validateSubject() && validateDescription()){
+            printForm();
+        }
+        else{
+            console.log("nope");
+        }
+    }
+
     return <>
         <div className={styles.ContactComp}> 
             <p className={styles.contactparagraph}>Interested in hiring me for your project or just want to say hi?  <br /> 
                     You can fill in the contact form below <br /> or send me an email to&nbsp;
                     <a href="mailto:lamia_ouassaa@outlook.com">lamia_ouassaa@outlook.com</a>
             </p>           
-            <div className={styles.contactcontainer}>      
-                <form className={styles.contactform} id="contactForm" noValidate>
+            <div className={styles.contactcontainer}>  
+                <form className={styles.contactform} id="contactForm" >
                         <div className={styles.contactformheader}>
                             <label>
-                                <input type="text" id="name" className={styles.nom} required minLength="5" maxLength="30" placeholder="Name"/>
+                                <input type="text" id="name" className={styles.nom} required minLength="2" maxLength="30" placeholder="Name" /*onChange={validateName}*/ />
                             </label>
-                            <div id="nom-erreur" class="erreur"> </div>
                             <label>
-                                <input type="email" id="email" className={styles.courriel} required placeholder="Email"/>
+                                <input type="email" id="email" className={styles.courriel} required placeholder="Email" /*onChange={validateEmail}*//>
                             </label>
                         </div>
                         <label>
-                            <input type="text" id="subject" className={styles.sujet} required minLength="1" maxLength="30" placeholder="Subject"/>
+                            <input type="text" id="subject" className={styles.sujet} required minLength="1" maxLength="30" placeholder="Subject" /*onChange={validateSubject}*/ />
                         </label>
                         <label>
-                            <textarea className={styles.message} id="description" placeholder="Message" required minLength="1" maxLength="300"></textarea>    
+                            <textarea className={styles.message} id="description" placeholder="Message" required minLength="1" maxLength="500" /*onChange={validateDescription}*/ ></textarea>    
                         </label>
-                        <input /*onClick={printForm}*/ onClick={validerNom} className={styles.contactsubmit} type="button" value="Send now"></input>
-                    
+                        <input onClick={validateInputs} className={styles.contactsubmit} type="button" value="Send now"></input>
+                        <div id="nomerreur" class="erreur"> </div>    
                 </form>
             </div>               
         </div>  
     </>
 }
-<script type="module" src='../validation.js'></script>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ export default function Home() {
         </div>   
         <div className="welcome">
             <Welcome />
+            <div id="nomerreur" class="erreur">sup </div>    
         </div>      
     </main>
 }
