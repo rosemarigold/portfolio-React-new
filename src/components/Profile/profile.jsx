0fb8659d2ef3f4e1497242988dbf7dc02acb3496
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-04-30 11:39:46 -0400

    Update 2022-04-30 11:39 AM

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -6,9 +6,11 @@ export default function Profile() {
             <img src={require('../../resources/totoro.gif')} alt="profile" className={styles.profilepicture} />   
 
             <div className={styles.profileintro}>
-                <h1>Hi!</h1>
-                <p>I'm Lamia Ouassaa, a graduating student in Computer Programming passionate about creating fun and interactive application!</p>
-                <span>Check out my projects!</span>     
+                <h1>Hi, I'm Lamia!</h1>
+                <div className={styles.dialogbox}>
+                    <p>I'm a graduating student passionate about creating fun and interactive application!</p>
+                    <p>Check out my projects!</p>  
+                </div>         
             </div>
         </div>
     </>
