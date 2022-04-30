e06584dfc5b86c6b042b9d576bf5347cd613e3a1
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-04-30 11:45:43 -0400

    Update 2022-04-30 11:45 AM

---
+++ src/components/Skill/skill.jsx
@@ -0,0 +1,9 @@
+import styles from './skill.module.css';
+
+export default function Skill() {
+    return <>
+        <div className={styles.skillcontainer}>  
+            
+        </div>
+    </>
+}
