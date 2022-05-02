d53140c21fde025c0e8f41a9141f88ddc0c5719b
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-05-02 11:54:07 -0400

    Update 2022-05-02 11:54 AM

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -20,14 +20,27 @@ export default function Skills() {
             </div>  
 
             <div className="pageslayout">
+                <div className="skillscontainer">
+                <Skill title="HTML, CSS &#38; JS" paragraph="Developed websites using Node.js, Express.js and React.">
 
-                <Skill title="Hi" paragraph="smt smt">
-                         
-                 </Skill>
-                 <div className={styles.imageContainer}>
-                        <Image source={image1}/>         
-                    </div>   
-                
+                </Skill>
+                <Skill title=".NET Framework" paragraph="Developed desktop applications in C# using WPF and WinForm.">
+                    
+                </Skill>
+                <Skill title="C# &#38; C++" paragraph="Built various academic project such as desktop applications and simple video games.">
+                    
+                </Skill>
+                <Skill title="Java" paragraph="Built mobile applications for Android using Android Studio.">
+                    
+                </Skill>
+                <Skill title="Swift" paragraph="Built mobile applications for iOS using Xcode.">
+                    
+                </Skill>
+                <Skill title="SQL Server" paragraph="Used to host websites and applications. Used MySQL, MongoDB &#38; Cassandra.">
+                    
+                </Skill>
+                  
+                </div> 
 
             </div> 
 
