4d6cca384a378e633f0ccdb6265f82a23c7902c9
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-05-07 09:47:33 -0400

    Update 2022-05-07 9:47 AM

--- src/components/ProjectMenu/projectmenu.jsx
+++ src/components/ProjectMenu/projectmenu.jsx
@@ -3,13 +3,11 @@ import styles from './projectmenu.module.css';
 export default function  ProjectMenu(props) {
     return <>
         <div className={styles.projectmenucontainer}>  
-            <div className={styles.projectinfo}>  
-                <div className={styles.title}>
-                    {props.title}
-                </div>
-                <div className={styles.paragraph}>
-                    {props.paragraph}
-                </div>
+            <div className={styles.title}>
+                {props.title}
+            </div>
+            <div className={styles.paragraph}>
+                {props.paragraph}
             </div>
             <div className={styles.image}>
                 <div className={styles.imageContainer}>{props.children}</div>           
