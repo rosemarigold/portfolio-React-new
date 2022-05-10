8d6b027ba2edfcf1fadcabfb59b320a9844f996b
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-05-10 19:03:51 -0400

    Update 2022-05-10 7:03 PM

--- src/components/ImageSlider/imageSlider.jsx
+++ src/components/ImageSlider/imageSlider.jsx
@@ -8,6 +8,10 @@ const ImageSlider = ({ slides }) => {
     const [current, setCurrent] = useState(0)
     const length = slides.length
   
+    const previousSlide = () => {
+        setCurrent(current === 0 ? length -1 : current - 1)
+    }
+
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
@@ -21,7 +25,7 @@ const ImageSlider = ({ slides }) => {
 
     return <>
     <section className={styles.slider}>
-        <FaArrowAltCircleLeft className={styles.leftArrow} />
+        <FaArrowAltCircleLeft className={styles.leftArrow} onClick={previousSlide} />
         <FaArrowAltCircleRight className={styles.rightArrow} onClick={nextSlide}/>
             {SliderData.map((slide, index) => {
                 return(
