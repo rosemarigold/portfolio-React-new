b5c23614d9ecdd512d7124a2f2ada40a1c3567c3
rosemarigold <97370537+rosemarigold@users.noreply.github.com>
2022-06-04 16:54:03 -0400

    Update 2022-06-04 4:53 PM

--- src/components/ContactMe/contactMe.jsx
+++ src/components/ContactMe/contactMe.jsx
@@ -1,9 +1,15 @@
+// Source: https://www.emailjs.com/docs/examples/reactjs/
+// Source: https://www.youtube.com/watch?v=jOviw8Ou_Yk&ab_channel=Zpunet
+// Source: https://fr.reactjs.org/docs/forms.html
+
 import styles from './contactMe.module.css';
+import React from 'react';
 import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default function Contact() {
+export default function ContactComponent(props) {
 
-    // get HTML element
+    // get the HTML element for the form
     const nameInput = useRef(null);
     const emailInput = useRef(null);
     const subjectInput = useRef(null);
@@ -11,12 +17,17 @@ export default function Contact() {
 
     // #1 verify name
     const [messageErreurName, setMessageErreurName] = useState('');
+
     const validateName = () => {
+
+        // if the input is valid, return true
         if (nameInput.current.validity.valid) {
-            setMessageErreurName('');
-            // if valid, return true
+
+            setMessageErreurName(''); // no error message to send
+            
             return true;
         }
+        // if not valid, return error message
         else {
             if (nameInput.current.validity.valueMissing) {
                 setMessageErreurName('Your name is missing.');
@@ -28,12 +39,17 @@ export default function Contact() {
     }   
     // #2 verify email 
     const [messageErreurEmail, setMessageErreurEmail] = useState('');
+
     const validateEmail = () => {
+
+        // if the input is valid, return true
         if (emailInput.current.validity.valid) {
-            setMessageErreurEmail('');
-            // if valid, return true
+
+            setMessageErreurEmail(''); // no error message to send
+          
             return true;
         }
+        // if not valid, return error message
         else {
             if (emailInput.current.validity.valueMissing) {
                 setMessageErreurEmail('Your email is missing.');
@@ -45,12 +61,17 @@ export default function Contact() {
     }
     // #3 verify subject
     const [messageErreurSubject, setMessageErreurSubject] = useState('');
+
     const validateSubject = () => {
+
+        // if the input is valid, return true
         if (subjectInput.current.validity.valid) {
-            setMessageErreurSubject('');
+
+            setMessageErreurSubject(''); // no error message to send
             // if valid, return true
             return true;
         }
+        // if not valid, return error message
         else {
             if (subjectInput.current.validity.valueMissing) {
                 setMessageErreurSubject('The subject is missing.');
@@ -62,12 +83,17 @@ export default function Contact() {
     }
     // #4 verify subject
     const [messageErreurDescription, setMessageErreurDescription] = useState('');
+
     const validateDescription = () => {
+
+        // if the input is valid, return true
         if (descriptionInput.current.validity.valid) {
+
             setMessageErreurDescription('');
             // if valid, return true
             return true;
         }
+        // if not valid, return error message
         else {
             if (descriptionInput.current.validity.valueMissing) {
                 setMessageErreurDescription('The description is missing.');
@@ -78,54 +104,62 @@ export default function Contact() {
         }
     }
 
-    // #5 submit
+    // reference to the form
+    const form = useRef();
+
+    // #5 submit form
     const [messageEmailSent, setMessageEmailSent] = useState('');
+
     const submitForm = (event) => {
+
         // to disable automatic refresh on page on submit event
         event.preventDefault(); 
        
-        //
+        // if all form inputs are valid, send the email
         if(validateName() && validateEmail() && validateSubject() && validateDescription()){
-            if (nameInput.current && emailInput.current && subjectInput.current && descriptionInput.current/* && 
-            event.currentTarget.checkValidity()*/) {
-            // print info
-            console.log("Name: " + nameInput.current.value);
-            console.log("Email: " + emailInput.current.value);
-            console.log("Email: " + subjectInput.current.value);
-            console.log("Description: " + descriptionInput.current.value);
+            if (nameInput.current && emailInput.current && subjectInput.current && descriptionInput.current) {
+ 
+            // function from EmailJs to send the email
+            emailjs.sendForm('service_ncf2w2w', 'template_cdc9cfy', form.current, 'r42Y-azQa8YNJiP4T')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+            
+            // print a confirmation message that the email was sent
+            setMessageEmailSent('Your email was sent!');
 
-            // empty inputs
+            // makes the form inputs empty
             nameInput.current.value = '';
             emailInput.current.value = '';
             subjectInput.current.value = '';
             descriptionInput.current.value = '';
-            
-            setMessageEmailSent('Your email was sent!');
-            }
         }
-    }
+    }   
+}
 
-    return <>
+    return (
         <div className={styles.ContactComp}> 
-            <p className={styles.contactparagraph}>Interested in hiring me for your project or just want to say hi?  <br /> 
-                    You can fill in the contact form below <br /> or send me an email to&nbsp;
-                    <a href="mailto:lamia_ouassaa@outlook.com">lamia_ouassaa@outlook.com</a>
-            </p>           
-            <div className={styles.contactcontainer}>  
-                <form className={styles.contactform} noValidate onSubmit={submitForm}>
+        <p className={styles.contactparagraph}>Interested in hiring me for your project or just want to say hi?  <br /> 
+                You can fill in the contact form below <br /> or send me an email to&nbsp;
+                <a href="mailto:lamia_ouassaa@outlook.com">lamia_ouassaa@outlook.com</a>
+        </p>  
+        <div className={styles.contactcontainer}>  
+                <form  ref={form} onSubmit={submitForm} className={styles.contactform} noValidate>
                         <div className={styles.contactformheader}>
                             <label>
-                                <input type="text" className={styles.nom} required minLength="1" maxLength="30" placeholder="Name" ref={nameInput} onChange={validateName} onBlur={validateName} />
+                                <input type="text" className={styles.nom} required minLength="1" maxLength="30" placeholder="Name" ref={nameInput} onChange={validateName} onBlur={validateName} name="fullname" />
                             </label>
                             <label>
-                                <input type="email" className={styles.courriel} required placeholder="Email" ref={emailInput} onChange={validateEmail} onBlur={validateEmail} />
+                                <input type="email" className={styles.courriel} required placeholder="Email" ref={emailInput} onChange={validateEmail} onBlur={validateEmail} name="email"/>
                             </label>
                         </div>
                         <label>
-                            <input type="text" className={styles.sujet} required minLength="1" maxLength="30" placeholder="Subject" ref={subjectInput} onChange={validateSubject} onBlur={validateSubject} />
+                            <input type="text" className={styles.sujet} required minLength="1" maxLength="30" placeholder="Subject" ref={subjectInput} onChange={validateSubject} onBlur={validateSubject} name="subject" />
                         </label>
                         <label>
-                            <textarea className={styles.message} placeholder="Message" required minLength="1" maxLength="500" ref={descriptionInput} onChange={validateDescription} onBlur={validateDescription} ></textarea>    
+                            <textarea className={styles.message} placeholder="Message" required minLength="1" maxLength="500" ref={descriptionInput} onChange={validateDescription} onBlur={validateDescription} name="message"></textarea>    
                         </label>
                         <input className={styles.contactsubmit} type="submit" value="Send now"></input>
                         {messageErreurName !== '' &&
@@ -144,7 +178,7 @@ export default function Contact() {
                             <div className={styles.emailSent}>{messageEmailSent}</div>
                         }
                 </form>
-            </div>               
-        </div>  
-    </>
+            </div>   
+        </div>   
+    );
 }
